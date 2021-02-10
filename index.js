ec385d416e90dd2b0ad1d8fd1f47ac21208c65bc
mitudas <78866948+mitudas@users.noreply.github.com>
2021-02-11 01:56:02 +0600

    Event Listener
     Event Listener function uses

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
  
-function addstyle()
+var myvar=document.querySelector("#pid"); //first para select
+function addstyle() //addstyle function
 {
 
-	var myvar=document.querySelector("#pid");
+	
 	
 	myvar.classList.add("para");
 }
 
-function removestyle()
+function removestyle()//removestyle function
 {
 
 	var myvar=document.querySelector("#pid");
-	myvar.classList.remove("para");
+	myvar.classList.remove("para");//css class use
 }
 
 
+ var myvar2=document.querySelector("#pid2");//second para select
+  myvar2.addEventListener("mouseover",function()// addlistener function & anonimas function
+
+ {
+ 	myvar2.classList.add("para2");
+ }
+
+);
+
+    myvar2.addEventListener("mouseout",function()
+
+ {
+ 	myvar2.classList.remove("para2");
+ }
+
+);
 var photos=["image/1.jpg","image/2.jpg", "image/3.jpg", "image/4.jpg","image/5.jpg","image/6.jpg","image/7.jpg","image/8.jpg","image/9.jpg","image/10.jpg","image/11.jpg","image/12.jpg","image/13.jpg"];
-var imgTag=document.querySelector("img");
+var imgTag=document.querySelector("img");//imag tag select
 
 var count=0;
 
- function next()
+ function next()//next function
  {
   count++;
   if(count>=photos.length){
@@ -32,7 +49,7 @@ var count=0;
  }
 }
 
-function prev()
+function prev()//previous function
 
 
 {
