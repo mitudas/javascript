439a8ac7de4de42ae1b3def75d7517401fa0b06e
mitudas <78866948+mitudas@users.noreply.github.com>
2021-02-10 22:23:35 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+ 
+var photos=["image/1.jpg","image/2.jpg", "image/3.jpg", "image/4.jpg","image/5.jpg","image/6.jpg","image/7.jpg","image/8.jpg","image/9.jpg","image/10.jpg","image/11.jpg","image/12.jpg","image/13.jpg"];
+var imgTag=document.querySelector("img");
+
+var count=0;
+
+ function next()
+ {
+  count++;
+  if(count>=photos.length){
+  	count=0;
+  	imgTag.src=photos[count];
+  }
+  else{
+  imgTag.src=photos[count];
+ }
+}
+
+function prev()
+
+
+{
+count--;
+  if(count<0){
+  	count=photos.length-1;
+  	imgTag.src=photos[count];
+  }
+  else{
+  imgTag.src=photos[count];
+ }
+}
