f3bae8e5bedc321f4eba16ed8cafa2d3d895910e
mitudas <78866948+mitudas@users.noreply.github.com>
2021-02-11 03:17:02 +0600

    Audio & button
    Audio & button

--- index.js
+++ index.js
@@ -62,3 +62,30 @@ count--;
   imgTag.src=photos[count];
  }
 }
+ //Event Listeners with multiple elements
+for(var i=0; i<3; i++){
+ document.querySelectorAll(".one")[i].addEventListener("click",function(){
+ 	var text=this.innerHTML;
+  console.log(text);
+  
+switch(text){
+
+  	case "Music1":
+  	             var audio=new Audio("sounds/musicOne.mp3");
+  	             audio.play();
+  	             break;
+    case "Music2":
+  	             var audio=new Audio("sounds/musicTwo.mp3");
+  	             audio.play();
+  	             break;
+     case "Music3":
+  	             var audio=new Audio("sounds/musicthree.mp3");
+  	             audio.play();
+  	             break;
+
+
+  }
+
+ });
+}
+
