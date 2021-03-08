ea10fddbbd0e5aba4e21adeb78e47d625a0557ce
mitudas <78866948+mitudas@users.noreply.github.com>
2021-03-08 20:48:26 +0600

    Array.js
    Array.js

---
+++ Array.js
@@ -0,0 +1,133 @@
+//Example-1
+//var javaScript={
+
+	//name: "javaScript",
+	//libraries:["React", "Angular", "vue"],
+	//printlibraries: function()
+	//{
+		//var self=this;
+		//this.libraries.forEach(function(a)
+		//{
+			//console.log(self.name+"Loves"+a);
+		//});
+	//},
+//};
+//javaScript.printlibraries();
+
+// Example-2
+//var javaScript={
+
+	//name: "javaScript",
+	//libraries:["React", "Angular", "vue"],
+	//printlibraries: function()
+	//{
+		
+		//this.libraries.forEach((a)=>
+		//{
+			//console.log(this.name+"Loves"+a);
+		//});
+	//},
+//};
+//javaScript.printlibraries();
+
+// Example-3
+
+ //const SearchInput=document.querySelector(".search");
+ //const Display=document.querySelector(".result");
+ // const Thanks=document.querySelector(".thanks");
+
+  //function show()
+  //{
+  //	console.log(this);
+  //	Display.innerHtml=this.value;
+
+  	//setTimeout(function(){
+  		//Thanks.innerHtml=this.value;
+  	//},1000);
+ // }
+
+ // SearchInput.addEventListener("keyup",show);
+
+ //example-04
+
+ //var myvar=[]; //0, null, "", false, undefined, NaN ai gulo chara baki sobe true return korbe
+
+ //if(myvar)
+ //{
+ 	//console.log("It is true");
+// }
+
+// else{
+ 	//console.log("It is false");
+
+ //}
+
+ // example-05 Ternary Operator
+
+ //var age=15;
+
+ //var type= age>=18? "adult":age<10? "child":"young";
+ //console.log(type);
+
+  // Example-6 Array find() method
+
+
+  //var numbers=[1,2,3,4,5,6,10];
+
+  //var result=numbers.find(function(value){
+
+  	//return value>6;
+  //});
+
+  //console.log(result);
+
+  //Example-7 Array findIndex() method
+
+  //var numbers=[1,2,3,4,5,6,10];
+
+ //var result= numbers.findIndex((currentValue, index, arr)=>{
+
+  	//return !(currentValue%2);
+ // });
+
+ // console.log(result);
+
+ // example-8 Array filter() method
+
+ //var numbers=[1,2,3,4,5,6,10];
+
+ //var value=numbers.filter((currentValue, index,arr)=>{
+
+ 	//return currentValue>4;
+ //});
+
+ //console.log(value);
+
+
+ //example-09  Array splice() method
+ //var numbers=[1,2,3,4,5,6,10];
+ //var value=numbers.splice(-1,3,100,300);
+ //var value1=numbers.slice(2,4);
+
+
+ //console.log(value);
+ //console.log(value1);
+
+ //console.log(numbers);
+
+ //Example-10 Array concat() method 
+
+ //var num1=[1,2,3,4];
+ //var num2=[100,200,300,400];
+
+ //var result=num1.concat(num2);
+
+ //console.log(result);
+
+ // example-11 - Array push() method
+
+
+
+
+
+
